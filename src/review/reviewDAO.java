d1d10f76b49fc5dbf6b45be141e94294382c75e0
eogus0512 <[eogus0512@gmail.com]>
2021-08-23 02:58:38 +0900

    FEAT:다시 복구...

--- src/review/reviewDAO.java
+++ src/review/reviewDAO.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 public class reviewDAO {
 
     public static ArrayList<String[]> getStoreReview(Integer restID) {
-        String SQL = "SELECT userID, contents, score FROM REVIEW WHERE restID = ?";
+        String SQL = "SELECT reviewID, userID, contents, score, createdAt FROM REVIEW WHERE restID = ?";
         ArrayList<String[]> review = new ArrayList<String[]>();
         Connection conn = null;
         PreparedStatement pstmt = null;
@@ -19,10 +19,12 @@ public class reviewDAO {
             pstmt.setInt(1, restID);
             rs = pstmt.executeQuery();
             while(rs.next()) {
-                String[] reviewValue = new String[3];
+                String[] reviewValue = new String[5];
                 reviewValue[0] = rs.getString(1);
                 reviewValue[1] = rs.getString(2);
                 reviewValue[2] = rs.getString(3);
+                reviewValue[3] = rs.getString(4);
+                reviewValue[4] = rs.getString(5);
                 review.add(reviewValue);
             }
         } catch (Exception e) {
@@ -68,9 +70,8 @@ public class reviewDAO {
         return avgScore;
     }
 
-    public static ArrayList<String[]> myReview(String userID) {
-        String SQL = "SELECT restID, contents, score, reviewID FROM REVIEW WHERE userID = ?";
-        ArrayList<String[]> review = new ArrayList<String[]>();
+    public static boolean ifReviewed(String userID, String reviewID) {
+        String SQL = "SELECT reviewRecommendID FROM REVIEWRECOMMEND WHERE userID = ? and reviewID = ?";
         Connection conn = null;
         PreparedStatement pstmt = null;
         ResultSet rs = null;
@@ -78,52 +79,67 @@ public class reviewDAO {
             conn = databaseUtil.getConnection();
             pstmt = conn.prepareStatement(SQL);
             pstmt.setString(1, userID);
+            pstmt.setString(2, reviewID);
             rs = pstmt.executeQuery();
-            while(rs.next()) {
-                String[] reviewValue = new String[4];
-                reviewValue[0] = rs.getString(1);
-                reviewValue[1] = rs.getString(2);
-                reviewValue[2] = rs.getString(3);
-                reviewValue[3] = rs.getString(4);
-                review.add(reviewValue);
-            }
+            if(rs.next()) return true;
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return review;
+        return false;
     }
 
-    public static String getRestName(String restID) {
-        String SQL = "SELECT name FROM RESTAURANT WHERE serialNum = ?";
+    public static int reviewRecommend(String reviewID, String userID) {
+        String SQL = "INSERT INTO REVIEWRECOMMEND(reviewID, userID) VALUES(?, ?)";
         Connection conn = null;
-        String name = null;
         PreparedStatement pstmt = null;
         ResultSet rs = null;
         try {
             conn = databaseUtil.getConnection();
             pstmt = conn.prepareStatement(SQL);
-            pstmt.setString(1, restID);
-            rs = pstmt.executeQuery();
-            if(rs.next()) {
-                name = rs.getString(1);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return name;
-    }
-
-    public static int deleteReview(String reviewID) {
-        String SQL = "DELETE TABLE FROM REVIEW WHERE reviewID = ?";
-
-        try {
-            Connection conn = databaseUtil.getConnection();
-            PreparedStatement pstmt = conn.prepareStatement(SQL);
             pstmt.setString(1, reviewID);
+            pstmt.setString(2, userID);
             return pstmt.executeUpdate();
         } catch (Exception e) {
             e.printStackTrace();
         }
         return -1;
     }
+
+    public static int reviewRecommendCancel(String reviewID, String userID) {
+        String SQL = "DELETE FROM REVIEWRECOMMEND WHERE reviewID = ? and userID = ?";
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            conn = databaseUtil.getConnection();
+            pstmt = conn.prepareStatement(SQL);
+            pstmt.setString(1, reviewID);
+            pstmt.setString(2, userID);
+            return pstmt.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    public static String reviewCount(String reviewID) {
+        String SQL = "SELECT COUNT(*) FROM REVIEWRECOMMEND WHERE reviewID = ?";
+        String count = null;
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            conn = databaseUtil.getConnection();
+            pstmt = conn.prepareStatement(SQL);
+            pstmt.setString(1, reviewID);
+            rs = pstmt.executeQuery();
+            if(rs.next()) {
+                count = rs.getString(1);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return count;
+    }
+
 }
